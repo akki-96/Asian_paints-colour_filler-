ff7c528a0269402eb0f916d3d2819819527a8d6f
akki-96 <75878738+akki-96@users.noreply.github.com>
2021-02-13 19:45:52 +0530

    modified

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 
+let count=0;
 let asianBucket = document.getElementById("container");
 for (let i = 1; i <= 9; i++) {
     let ele = document.createElement("div");
-    ele.setAttribute("id","cell_"+i );
-    ele.setAttribute("class" ,"block");
-    ele.innerHTML=i;
+    ele.setAttribute("id", "cell_" + i);
+    ele.setAttribute("class", "block");
+    ele.innerHTML = i;
     asianBucket.appendChild(ele);
 }
-// setAttributes(div, {"class":"block", "id": i, "value": i, })
 
-function applyColor(){
-    let colorName=document.getElementById("colour_id").value
-    //console.log(colorName);
-    let blockId=parseInt(document.getElementById("block_id").value)
-    //console.log(blockId);
-    if(colorName!="" && blockId!="")
-    {
-        let element = document.getElementById("cell_"+blockId);
+
+function applyColor() {
+    if( document.getElementById("block_id").value != "")
+      rest();
+
+    let colorName = document.getElementById("colour_id").value
+        
+    let blockId = parseInt(document.getElementById("block_id").value)
+    
+     
+
+    if (colorName != "" && blockId != "") {
+        let element = document.getElementById("cell_" + blockId);
         element.style.backgroundColor = colorName;
-       // element.innerHTML="";
-      
+        document.getElementById("block_id").value = "";
+        document.getElementById("colour_id").value = "";
     }
-    else{
+
+    else {
         return;
     }
 }
 
-function resetColor(){
+function resetColor() {
     document.querySelectorAll(".block").forEach(block => (block.style.backgroundColor = "transparent"));
     document.getElementById("block_id").value = "";
     document.getElementById("colour_id").value = "";
-//     for (let i = 1; i <= 9; i++) {
-//         let ele = document.getElementById("cell_"+i);
-//         ele.innerHTML=i;
-//  }
+}
+
+function rest(){
+    document.querySelectorAll(".block").forEach(block => (block.style.backgroundColor = "transparent"));
 }
